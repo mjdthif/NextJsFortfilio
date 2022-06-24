c0913e61ebe956e8cdf17ad8a85e5d004418e466
MJDJ <mjd.thif@icloud.com>
2022-06-25 00:18:34 +0200

    fixxed sooome bugs

--- Components/socialmediaQr/ContactQR.js
+++ Components/socialmediaQr/ContactQR.js
@@ -1,18 +1,30 @@
 
 
+import Image from 'next/image';
+import Link from 'next/link';
 import ProjectNav from '../Projects/ProjectNav/ProjectNav' ;
 import meBirlingym from "../../public/Assests/webP/meBirlingym.webp";
 import SocialMediaQr from './SocialMediaQr';
 import style from './ContactQR.module.scss';
+
 
 const QrContact = () => {
   return (
       <div className='container'>
               <section className={style.businessCard}>
                     <article className={style.profile}>
-                     <a href='/'> <img src={meBirlingym}/></a>
+            <Link href="/">
+                  <a> 
+                    <Image 
+                      width='100px'
+                      height='100px'
+                      src={meBirlingym}
+                    />
+                </a>
+           </Link>
+         
                       <div>
-                        <h2>Mjd Thif</h2>
+                        <h1>Mjd Thif</h1>
                         <p>Web Develover</p>
                       </div>
                     </article>
--- Components/socialmediaQr/SocialMediaQr.js
+++ Components/socialmediaQr/SocialMediaQr.js
@@ -1,8 +1,12 @@
 
-import {BsGithub, BsLinkedin, BsSpotify, BsInstagram,BsTwitter, BsPinterest} from 'react-icons/bs';
+import {BsGithub, BsLinkedin, BsSpotify, BsInstagram,BsTwitter, BsPinterest, BsGlobe2} from 'react-icons/bs';
+import{FaDownload} from 'react-icons/fa';
 import {SiCodewars} from 'react-icons/si';
 import{MdOutlineMailOutline} from 'react-icons/md';
 import{BiPhoneCall} from'react-icons/bi';
+// import MyENGCV from '../../public/Assests/cvFiles/ENGCV.pdf'
+
+
 import style from './ContactQR.module.scss';
 
 
@@ -12,19 +16,18 @@ const SocialMediaQr = () => {
   return (
  
    <div className={style.businessCard}>
-        <a href='tel:00469871260' target="_blank" alt='Call' ><BiPhoneCall/></a>
-        <a href='mailto:mjd.taif@levantisk.com' target="_blank"><MdOutlineMailOutline/></a>
-        <a href="https://www.linkedin.com/in/mjd-thif/"  target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/mjdthif" target='_blank' ><BsGithub/></a>
-        <a href="https://www.codewars.com/users/mjdthif"  target='_blank'> <SiCodewars/></a>
-        <a href="https://www.instagram.com/hammuarabi/"  target='_blank'> <BsInstagram/></a>
-        <a href="https://www.pinterest.se/mjdreklam/_created/"  target='_blank'> <BsPinterest/></a>
-        <a href="" target='_blank'> <BsSpotify/></a>
-        <a href="https://twitter.com/DJohanberg" target='_blank'> <BsTwitter/></a>
+        <a href='tel:00469871260' target="_blank" className={style.cardLinks} alt='Mobile'><BiPhoneCall/><h1>Mobile</h1></a>
+        <a href='mailto:mjd.taif@levantisk.com' target="_blank" className={style.cardLinks} alt='Email'><MdOutlineMailOutline/><h1>Email</h1></a>
+        <a href="https://www.linkedin.com/in/mjd-thif/"  target='_blank' className={style.cardLinks} alt='LinkedIn'><BsLinkedin/><h1>LinkedIn</h1></a>
+        <a href="https://www.mjdwebstudio.com/"  target='_blank' className={style.cardLinks} alt='Portfilio'> <BsGlobe2/><h1>Portfilio</h1></a>
+        <a href="https://www.codewars.com/users/mjdthif"  target='_blank' className={style.cardLinks} alt='Code Wars'> <SiCodewars/><h1>Code Wars</h1></a>
+        <a href="https://www.instagram.com/hammuarabi/"  target='_blank' className={style.cardLinks} alt='Instagram'> <BsInstagram/><h1>Instagram</h1></a>
+        {/* <a  href={MyENGCV} download className={style.cardLinks} alt='Call'> <FaDownload/><h1> Download CV</h1></a> */}
+        <a href="https://www.pinterest.se/mjdreklam/_created/"  target='_blank' className={style.cardLinks} alt='Pinterest'> <BsPinterest/> <h1>Pinterest</h1></a>
+        <a href="https://github.com/mjdthif" target='_blank' className={style.cardLinks} alt='Github'><BsGithub/><h1>Github</h1></a>
+        <a href="" target='_blank' className={style.cardLinks} alt='Spotify'> <BsSpotify/><h1>Spotify</h1></a>
+        <a href="https://twitter.com/DJohanberg" target='_blank' className={style.cardLinks} alt='Twitter'> <BsTwitter/><h1>Twitter</h1></a>
   </div>
-
-  
-
 
   )
 }
