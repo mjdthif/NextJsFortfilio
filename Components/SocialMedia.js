ba65012a24c35fa44c5c1db38c3193335505978c
MJDJ <mjd.thif@icloud.com>
2022-06-28 09:55:55 +0200

    fixed sm

--- Components/SocialMedia.js
+++ Components/SocialMedia.js
@@ -14,15 +14,15 @@ const SocialMedia = ({openSocial}) => {
   return (
     <div className={openSocial ? showSocial : hideSocial } >
       <div className={sociaStyle.socialContainer}>
-        <a href='tel:00469871260' target="_blank" alt='Call' ><BiPhoneCall/></a>
-        <a href='mailto:mjd.taif@levantisk.com' target="_blank"><MdOutlineMailOutline/></a>
-        <a href="https://www.linkedin.com/in/mjd-thif/"  target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/mjdthif" target='_blank' ><BsGithub/></a>
-        <a href="https://www.codewars.com/users/mjdthif"  target='_blank'> <SiCodewars/></a>
-        <a href="https://www.instagram.com/hammuarabi/"  target='_blank'> <BsInstagram/></a>
-        <a href="https://www.pinterest.se/mjdreklam/_created/"  target='_blank'> <BsPinterest/></a>
+        <a href='tel:00469871260' target="_blank" rel='noreferrer'  alt='Call' ><BiPhoneCall/></a>
+        <a href='mailto:mjd.taif@levantisk.com' target="_blank" rel='noreferrer'><MdOutlineMailOutline/></a>
+        <a href="https://www.linkedin.com/in/mjd-thif/"  target='_blank' rel='noreferrer'><BsLinkedin/></a>
+        <a href="https://github.com/mjdthif" target='_blank' rel='noreferrer' ><BsGithub/></a>
+        <a href="https://www.codewars.com/users/mjdthif"  target='_blank' rel='noreferrer'> <SiCodewars/></a>
+        <a href="https://www.instagram.com/hammuarabi/"  target='_blank' rel='noreferrer'> <BsInstagram/></a>
+        <a href="https://www.pinterest.se/mjdreklam/_created/"  target='_blank' rel='noreferrer'> <BsPinterest/></a>
         <a href="" target='_blank'> <BsSpotify/></a>
-        {/* <a href="https://twitter.com/DJohanberg" target='_blank'> <BsTwitter/></a> */}
+        {/* <a href="https://twitter.com/DJohanberg" target='_blank' rel='noreferrer'> <BsTwitter/></a> */}
       </div>
       
  
