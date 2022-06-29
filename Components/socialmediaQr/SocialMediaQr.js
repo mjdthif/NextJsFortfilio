461976c56d0dc62b6cad58ced66d4339b65cc926
MJDJ <mjd.thif@icloud.com>
2022-06-29 08:22:13 +0200

    fixd cvCompos

--- Components/socialmediaQr/SocialMediaQr.js
+++ Components/socialmediaQr/SocialMediaQr.js
@@ -22,7 +22,7 @@ const SocialMediaQr = () => {
         <a href="https://www.mjdwebstudio.com/"  target='_blank' className={style.cardLinks} alt='Portfilio'> <BsGlobe2/><h1>Portfilio</h1></a>
         <a href="https://www.codewars.com/users/mjdthif"  target='_blank' className={style.cardLinks} alt='Code Wars'> <SiCodewars/><h1>Code Wars</h1></a>
         <a href="https://www.instagram.com/hammuarabi/"  target='_blank' className={style.cardLinks} alt='Instagram'> <BsInstagram/><h1>Instagram</h1></a>
-        <a  href={MyENGCV} download className={style.cardLinks} alt='Call'> <FaDownload/><h1> Download CV</h1></a>
+        {/* <a  href={MyENGCV} download className={style.cardLinks} alt='Call'> <FaDownload/><h1> Download CV</h1></a> */}
         <a href="https://www.pinterest.se/mjdreklam/_created/"  target='_blank' className={style.cardLinks} alt='Pinterest'> <BsPinterest/> <h1>Pinterest</h1></a>
         <a href="https://github.com/mjdthif" target='_blank' className={style.cardLinks} alt='Github'><BsGithub/><h1>Github</h1></a>
         <a href="" target='_blank' className={style.cardLinks} alt='Spotify'> <BsSpotify/><h1>Spotify</h1></a>
