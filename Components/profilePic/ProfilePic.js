512663f79566f72178c9bbdec395b58dc90a61b4
MJDJ <mjd.thif@icloud.com>
2022-07-06 20:52:32 +0200

    fixed some bugs

--- Components/profilePic/ProfilePic.js
+++ Components/profilePic/ProfilePic.js
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import style from "./profilePic.module.scss";
-import profileImage from "../../public/Assests/webP/meBirlingym.webp";
+import meUppsala from "../../public/Assests/webP/meUppsala.webp";
+import meStockholm from "../../public/Assests/webP/meStockholm.webp";
+import meBirlin from "../../public/Assests/webP/meBirlin.webp";
 
 const ProfilePic = () => {
   return (
-  
-      <Image
-        src={profileImage}
-        width={"300px"}
+    <>
+       <div className={style.profilePic}>
+         <Image
+        src={meUppsala}
+        width={"150px"}
         height={"200px"}
         alt="Mjd Profile picture"
-        layout="responsive"
+      
+        priority={true}
       />
+         <Image
+        src={meStockholm}
+        width={"200px"}
+        height={"300px"}
+        alt="Mjd Profile picture"
+   
+        priority={true}
+      />
+         <Image
+        src={meBirlin}
+        width="250px"
+        height={"350px"}
+        alt="Mjd Profile picture"
+      
+        priority={true}
+      />
+      
+     
+
+    </div>
+    
+    </>
+ 
+   
 
   );
 };
